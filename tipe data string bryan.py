4e764b084f014e13adb904112ba602a3cfdbd902
melvin1797 <bryannkayy@gmail.com>
2024-09-27 11:04:39 +0700

    Add files via upload

---
+++ tipe data string bryan.py
@@ -0,0 +1,13 @@
+nama_depan = "Bryan"
+nama_belakang = 'Melvin Fahrezi'
+nama_lengkap = nama_depan + '' + nama_belakang
+usia = 18
+alamat = 'Belitung timur'
+kata_mutiara = "Jangan lupa untuk tidur"
+
+print(nama_lengkap, '(' + usia + ')', ', ', 'dari', alamat, ', kata mutiara:', kata_mutiara)
+
+print('\nTipe dari nama_lengkap:', type(nama_lengkap))
+print('Tipe dari usia:', type(usia))
+print(',Tipe dari alamat:', type(alamat))
+print('Tipe dari kata_mutiara:', type(kata_mutiara))
